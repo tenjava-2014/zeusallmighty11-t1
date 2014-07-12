fdc51ba0015ff5d0c64f19cf54c98d70f91236ac
Zeus <admin@mccoding.us>
2014-07-11 20:57:59 -0400

    Changed object instance checks a bit

--- src/main/java/com/tenjava/entries/zeusallmighty11/t1/util/Messenger.java
+++ src/main/java/com/tenjava/entries/zeusallmighty11/t1/util/Messenger.java
@@ -1,41 +1,64 @@
 package com.tenjava.entries.zeusallmighty11.t1.util;
 
 
-import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.entity.Player;
+import com.tenjava.entries.zeusallmighty11.t1.FancyTransit;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 
 
 public class Messenger
 {
 
 
-    private static final String PREFIX = "&3[&cBLAH&3]&r ";
+    private static final String PREFIX = "&3[&dFancyTransit&3]&r ";
 
 
 
 
+    /**
+     * Sends message(s) (with color/format support) to receiver
+     *
+     * @param o - Receiver of the message. Either a CommandSender instance, otherwise sent to console.
+     * @param msg - The message(s) to send
+     */
     public static void sendMessage(Object o, String... msg)
     {
         StringBuilder sb = new StringBuilder();
 
         for (String s : msg)
         {
-            sb.append(PREFIX).append(s).append("\n");
+            sb.append(PREFIX).append(ChatUtil.colorize(s)).append("\n");
         }
 
 
-        if (o instanceof Player)
+        if (o instanceof CommandSender)
         {
-            ((Player) o).sendMessage(sb.toString());
-        }
-        else if(o instanceof ConsoleCommandSender)
-        {
-            ((ConsoleCommandSender)o).sendMessage(sb.toString());
+            ((CommandSender) o).sendMessage(sb.toString());
         }
         else
         {
-
+            FancyTransit.getInstance().getLogger().info(sb.toString());
         }
+    }
+
+
+
+
+    /**
+     * Broadcasts the message across the server (With color support)
+     *
+     * @param msg - Message to send
+     */
+    public static void announce(String... msg)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : msg)
+        {
+            sb.append(ChatUtil.colorize(PREFIX)).append(ChatUtil.colorize(s)).append("\n");
+        }
+
+        Bukkit.broadcastMessage(sb.toString());
     }
 
 
